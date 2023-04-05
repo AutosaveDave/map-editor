0590d6801d060c03ab4347ec9f7726f63dad05a7
AutosaveDave <106124456+dccoppock@users.noreply.github.com>
2023-04-05 11:59:25 -0400

    Updates

--- src/components/lighting/LightPoint.js
+++
@@ -1,8 +0,0 @@
-// Light source component for development purposes
-import React from "react";
-
-const LightPoint = () => {
-    <pointLight position={[12, 12, 12]} />
-}
-
-export default LightPoint;
